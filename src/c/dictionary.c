4d33410ad952e0628a00c923a5b3530273580c81
otabat <otabat@gmail.com>
2017-05-16 00:57:37 +0900

    Fix wrong function declarations

--- src/c/dictionary.c
+++ src/c/dictionary.c
@@ -12,8 +12,9 @@ extern khash_t(StringPairTable)* get_kl_dictionary_table
 extern void set_kl_dictionary_table (KLObject* dictionary_object,
                                      khash_t(StringPairTable)* table);
 extern bool is_kl_dictionary (KLObject* object);
-inline long get_dictionary_count (Dictionary* dictionary);
-inline KLObject* get_kl_dictionary_count (KLObject* dictionary_object);
-inline KLObject* get_dictionary_keys (Dictionary* dictionary);
-inline KLObject* get_kl_dictionary_keys (KLObject* dictionary_object);
-inline KLObject* get_kl_dictionary_values (KLObject* dictionary_object);
+extern long get_dictionary_count (Dictionary* dictionary);
+extern KLObject* get_kl_dictionary_count (KLObject* dictionary_object);
+extern KLObject* get_dictionary_keys (Dictionary* dictionary);
+extern KLObject* get_kl_dictionary_keys (KLObject* dictionary_object);
+extern KLObject* get_dictionary_values (Dictionary* dictionary);
+extern KLObject* get_kl_dictionary_values (KLObject* dictionary_object);
