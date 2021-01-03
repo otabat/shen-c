8b2a8effdb836e8649150933e6665e99ea03d7ab
Felix Riedel <felix.riedel@gmail.com>
2021-01-03 11:38:38 +0000

    Automatically set home path on linux

--- src/c/main.c
+++ src/c/main.c
@@ -2,16 +2,43 @@
 
 #include "init.h"
 
+#ifdef __linux__
+#include <unistd.h>
+#include <libgen.h>
+#include <linux/limits.h>
+
+char exe_path[PATH_MAX];
+
+char *get_home_path() {
+  char *path = getenv("SHEN_C_HOME");
+  if (path) return path;
+
+  if (readlink("/proc/self/exe", exe_path, PATH_MAX) != -1)
+  {
+    dirname(exe_path);
+    strcat(exe_path, "/../");
+    return exe_path;
+  }
+  return NULL;
+}
+
+#else
+char *get_home_path() {
+  return getenv("SHEN_C_HOME");
+}
+#endif
+
 int main (int argc, char** argv)
 {
   GC_init();
 
-  char* home_path = getenv("SHEN_C_HOME");
+  char* home_path = get_home_path();
 
-  if (is_null(home_path))
+  if (is_null(home_path))  {
     throw_kl_exception("SHEN_C_HOME environment variable is not set\n"
                        "Set the variable to the Shen-C root directory\n"
                        "ex) export SHEN_C_HOME=/home/user/shen-c");
+  }
 
   initialize_and_run_shen_repl(home_path, argc, argv);
 
